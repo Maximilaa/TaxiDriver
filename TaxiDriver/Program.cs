dde1bfb970a4d33e9aea67328d9865e82e989623
fitrotulf <fitrotulfaizah@students.amikom.ac.id>
2023-05-09 17:40:06 +0700

    Add project files.

---
+++ TaxiDriver/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaxiDriver TaxiDriver = new TaxiDriver();
+
+            TaxiDriver.DriverName = "Jono";
+            TaxiDriver.OnDuty = true;
+            TaxiDriver.NumPassenger = 10;
+
+            TaxiDriver.TaxiInfo();
+            TaxiDriver.PickUpPassenger();
+            TaxiDriver.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
